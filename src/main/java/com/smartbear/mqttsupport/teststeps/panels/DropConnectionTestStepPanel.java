32c7a762021bba19bcaeea2b471563126bb6a726
Dmitry Aleshin <dmitry.aleshin@smartbear.com>
2018-03-20 09:13:43 +0300

    Fix drop panels help button

--- src/main/java/com/smartbear/mqttsupport/teststeps/panels/DropConnectionTestStepPanel.java
+++ src/main/java/com/smartbear/mqttsupport/teststeps/panels/DropConnectionTestStepPanel.java
@@ -1,15 +1,24 @@
 package com.smartbear.mqttsupport.teststeps.panels;
 
+import com.eviware.soapui.impl.support.actions.ShowOnlineHelpAction;
+import com.eviware.soapui.support.UISupport;
+import com.eviware.soapui.support.components.JXToolBar;
 import com.jgoodies.binding.PresentationModel;
 import com.smartbear.mqttsupport.teststeps.DropConnectionTestStep;
+import com.smartbear.ready.ui.style.GlobalStyles;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 public class DropConnectionTestStepPanel extends MqttConnectedTestStepPanel<DropConnectionTestStep> {
+
+    private static final String HELP_LINK = "/soapui/steps/mqtt-drop.html";
 
     public DropConnectionTestStepPanel(DropConnectionTestStep modelItem) {
         super(modelItem);
@@ -31,8 +40,18 @@ public class DropConnectionTestStepPanel extends MqttConnectedTestStepPanel<Drop
         root.add(propsPanel);
 
         JScrollPane scrollPane = new JScrollPane(root);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, GlobalStyles.getDefaultBorderColor()));
+        add(buildToolbar(), BorderLayout.NORTH);
         add(scrollPane);
         setPreferredSize(new Dimension(500, 300));
+    }
+
+    private JComponent buildToolbar() {
+        JXToolBar toolBar = UISupport.createToolbar();
+        ShowOnlineHelpAction showOnlineHelpAction = new ShowOnlineHelpAction(HELP_LINK);
+        JButton help = UISupport.createActionButton(showOnlineHelpAction, showOnlineHelpAction.isEnabled());
+        toolBar.addGlue();
+        toolBar.add(help);
+        return toolBar;
     }
 }
