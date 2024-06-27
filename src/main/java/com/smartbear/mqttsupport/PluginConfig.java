267460a63d5e99860bffba2dcc5f13605c1cde47
Konrad Krasocki <104936644+KodeRad@users.noreply.github.com>
2024-06-27 11:33:10 +0200

    Update src/main/java/com/smartbear/mqttsupport/PluginConfig.java
    Co-authored-by: Maciej Bartlodziej <121931242+macbarSB@users.noreply.github.com>

--- src/main/java/com/smartbear/mqttsupport/PluginConfig.java
+++ src/main/java/com/smartbear/mqttsupport/PluginConfig.java
@@ -5,7 +5,7 @@ import com.eviware.soapui.plugins.PluginConfiguration;
 import com.eviware.soapui.support.UISupport;
 @PluginConfiguration(groupId = "com.smartbear.plugins", name = "MQTT Support Plugin", version = "1.6.5-SNAPSHOT",
         autoDetect = true, description = "Adds MQTT TestSteps to ReadyAPI", minimumReadyApiVersion = "3.9.2",
-        infoUrl = "https://support.smartbear.com/readyapi/docs/integrations/mqtt.html?sbsearch=MQTT")
+        infoUrl = "https://support.smartbear.com/readyapi/docs/integrations/mqtt.html")
 public class PluginConfig extends PluginAdapter {
 
     public final static int DEFAULT_TCP_PORT = 1883;
