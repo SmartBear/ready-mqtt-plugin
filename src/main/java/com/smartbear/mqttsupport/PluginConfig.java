10761bce7957d0b61400980a28e5662b942a7ead
Piotr Darosz <piotr.darosz@smartbear.com>
2025-01-10 09:21:33 +0100

    RIA-23671 - Bouncy Castle update

--- src/main/java/com/smartbear/mqttsupport/PluginConfig.java
+++ src/main/java/com/smartbear/mqttsupport/PluginConfig.java
@@ -3,8 +3,8 @@ package com.smartbear.mqttsupport;
 import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 import com.eviware.soapui.support.UISupport;
-@PluginConfiguration(groupId = "com.smartbear.plugins", name = "MQTT Support Plugin", version = "1.6.8-SNAPSHOT",
-        autoDetect = true, description = "Adds MQTT TestSteps to ReadyAPI", minimumReadyApiVersion = "3.57.0",
+@PluginConfiguration(groupId = "com.smartbear.plugins", name = "MQTT Support Plugin", version = "1.6.9-SNAPSHOT",
+        autoDetect = true, description = "Adds MQTT TestSteps to ReadyAPI", minimumReadyApiVersion = "3.58.0",
         infoUrl = "https://support.smartbear.com/readyapi/docs/integrations/mqtt.html")
 public class PluginConfig extends PluginAdapter {
 
